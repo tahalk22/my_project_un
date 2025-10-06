96b175be51b208c0795c888729cac2aef1c1a546
tahalk22 <tahalaklok@gmail.com>
2025-10-06 17:27:48 +0200

    Disable Ziggy for Vercel preview

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,7 +8,7 @@ import 'vue3-toastify/dist/index.css';
 import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
-import { ZiggyVue } from '../../vendor/tightenco/ziggy';
+//import { ZiggyVue } from '../../vendor/tightenco/ziggy';
 
 // el +
 import ElementPlus from 'element-plus';
